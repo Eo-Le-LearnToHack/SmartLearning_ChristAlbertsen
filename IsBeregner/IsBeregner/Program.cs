c81035bdd946e6958c889922862630ba3a9d6e3a
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2022-07-06 21:55:13 +0200

    minor change

--- IsBeregner/IsBeregner/Program.cs
+++ IsBeregner/IsBeregner/Program.cs
@@ -2,7 +2,8 @@
 
 class IceCreamSeller
 {
-    private Dictionary<string, int> priceDict = new Dictionary<string, int>()
+
+    private Dictionary<string, int> priceDict = new()
     {
         ["1"] = 19,
         ["2"] = 25,
@@ -13,14 +14,42 @@ class IceCreamSeller
         ["nothing"] = 0
     };
 
+    private Dictionary<string, Func<string, bool>> validationFunctions = new() {
+        ["scoopCountValidation"] = userInput =>
+        {
+            int parsedValue = int.Parse(userInput);
+            return parsedValue > 0 && parsedValue < 5;
+        },
+        ["yesNoValidation"] = userInput =>
+        {
+            userInput = userInput.ToLower();
+            return userInput == "y" || userInput == "n";
+        }
+    };
+
     private int tap = 0;
+
+    public void TakeCustomerOrder()
+    {
+        Console.WriteLine("Hi, and welcome to our icecream shop. \n" +
+            "Would you like 1,2,3 or 4 scoops of icecream?");
+        string userInput = InputHandler(validationFunctions["scoopCountValidation"]);
+        PriceCalculator(userInput);
+
+        Console.WriteLine("Would you like guf? (y/n)");
+        userInput = InputHandler(validationFunctions["yesNoValidation"]);
+        if (userInput == "y") PriceCalculator("guf");
+
+        Console.WriteLine("Would you like a cream bun? (y/n)");
+        userInput = InputHandler(validationFunctions["yesNoValidation"]);
+        if (userInput == "y") PriceCalculator("flødebolle", true); else PriceCalculator("nothing", true);
+    }
 
     private string InputHandler(Func<string, bool> InputCriteria)
     {
-
         string userInput;
         bool criteriaEvaluation = false;
-
+        
         do
         {
             userInput = Console.ReadLine();
@@ -37,35 +66,7 @@ class IceCreamSeller
         if (printTotal) Console.WriteLine("prisen er " + tap + " kr.");
     }
 
-    public void TakeCustomerOrder()
-    {
-   
-        Func<string, bool> scoopCountValidation = userInput =>
-        {
-            int parsedValue = int.Parse(userInput);
-            return parsedValue > 0 && parsedValue < 5;
-        };
-        
-        Console.WriteLine("Hi, and welcome to our icecream shop. \n" +
-            "Would you like 1,2,3 or 4 scoops of icecream?");
-        string userInput = InputHandler(scoopCountValidation);
-        PriceCalculator(userInput);
 
-
-        Func<string, bool> yesNoValidation = x =>
-        {
-            x = x.ToLower();
-            return x == "y" || x == "n";
-        };
-        Console.WriteLine("Would you like guf? (y/n)");
-        userInput = InputHandler(yesNoValidation);
-        if (userInput == "y") PriceCalculator("guf");
-
-        Console.WriteLine("Would you like a cream bun? (y/n)");
-        userInput = InputHandler(yesNoValidation);
-        if (userInput == "y") PriceCalculator("flødebolle", true); else PriceCalculator("nothing", true);
-
-    }
 }
 
 class program
